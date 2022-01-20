0301fe04b0ac4aed6b9accd68492259fd39e9704
Lee Bloom <leebloomdev@gmail.com>
2022-01-19 19:46:14 -0500

    lesson 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Images To View';
+  photosTitle = 'Photos/Images';
   image1 = 'https://media.istockphoto.com/photos/pop-corn-and-on-red-armchair-cinema-picture-id1271522601?b=1&k=20&m=1271522601&s=170667a&w=0&h=azZRRchShbrwRgq58omc1HOYABnfDDOzXJatuaZrueQ=';
   image2 = 'https://st3.depositphotos.com/7755898/16521/v/600/depositphotos_165216992-stock-illustration-woman-doing-chores.jpg';
   image3 = 'https://s.hdnux.com/photos/47/46/46/10381742/3/rawImage.jpg';
